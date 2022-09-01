0b510a28649b8c7a433f8015b6ab2b3cc8ca5b35
Antoine A <aubin.antoine@lilo.org>
2022-09-01 10:45:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# UE5_Shaders
+ 
---
+++ Source/UE5_Shaders/UE5_ShadersGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_ShadersGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_SHADERS_API AUE5_ShadersGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
